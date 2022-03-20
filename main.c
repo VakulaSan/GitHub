333438a919284a8bcc03f6a808cde909299da709
VakulaSan <vakulasan@gmail.com>
2022-03-20 10:00:43 +0200

    Find a triplets with sum = 0

--- main.c
+++ main.c
@@ -1,13 +1,50 @@
 #include "stdio.h"
 
-int main(int argc, char const *argv[])
+int findTriplets(int arr[], int arr_length, int triplets[]);
+
+int main()
 {
-     float width, length, area;
-     printf("Please enter the width of your shape - \n");
-     scanf("%f", &width);
-     printf("Please enter length of rectangle- ");
-     scanf("%f", &length);
-     area = width + length;
-     printf("Area of rectangle is - %f square units\n", area);
+     int array[9] = {
+         1,
+         3,
+         -4,
+         2,
+         0,
+         10,
+         5,
+         -5};
+     int arr_length = 9;
+     int triplets[72];
+
+     findTriplets(array, arr_length, triplets);
+     printf("Triplets that in sum give \"0\" are: \n ");
+     for (int i = 0; i < arr_length; i++)
+     {
+          printf("%d", triplets[i]);
+     }
+
+     return 0;
+}
+
+int findTriplets(int arr[], int arr_length, int triplets[])
+{
+     for (int f = 0; f < arr_length - 2; f++) /* First level*/
+     {
+          for (int s = f + 1; s < arr_length - 1; s++) /* Second level*/
+          {
+               for (int t = s + 1; t < arr_length; t++) /* Third level*/
+               {
+                    int sum;
+                    if (!(sum = arr[f] + arr[s] + arr[t]))
+                    {
+                         static int i;
+
+                         triplets[i++] = arr[f];
+                         triplets[i++] = arr[s];
+                         triplets[i++] = arr[t];
+                    }
+               }
+          }
+     }
      return 0;
 }
