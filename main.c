bf1fbd01677378f955b591e16e5cb834cf4020d6
VakulaSan <vakulasan@gmail.com>
2022-03-22 10:48:17 +0200

    Find largest element in array V2

--- main.c
+++ main.c
@@ -3,28 +3,20 @@
 
 int main()
 {
-     int arr[] = {1, 3, 2, 1024, 3, 23, 54, 342, 22, 42, 51};
+     int arr[] = {1, 3, 2, 3, 166};
      int arr_length = sizeof(arr) / sizeof(arr[0]);
-     int pos = biggest_value(arr, arr_length);
-     printf("The largest element in array is - \"%d\".", pos);
+     int max = biggest_value(arr, arr_length);
+     printf("The largest element in array is - \"%d\".", max);
      return 0;
 }
 int biggest_value(int arr[], int arr_length)
 {
-     int left = 0,
-         right = arr_length - 1;
-     /* when there is left one element, it means right - left = 0*/
-     while (right - left)
+     int max = arr[0];
+     for (int i = 0; i < arr_length; i++)
      {
-          if (arr[left] > arr[right])
-          {
-               --right;
-          }
-          else
-          {
-               ++left;
-          }
+          if (arr[i] > max)
+               max = arr[i];
      }
-     /* It does not matter what we will return left or right because they are equal*/
-     return left;
+
+     return max;
 }
